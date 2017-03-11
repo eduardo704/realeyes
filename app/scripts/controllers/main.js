cd4386e245aa8db053c3bd5ec736d46f5496c2b3
Eduardo Vargas <eduardo.vargas@hipteam.io>
2017-03-11 12:23:31 +0100

    'new'

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name realeyesAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the realeyesAppApp
+ */
+angular.module('realeyesAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
