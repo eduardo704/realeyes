950baa1803257176fec5d0af9fec572b023cda0f
eduardo <eduardo704@gmail.com>
2017-03-12 14:31:54 +0100

    fixing build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2017-01-26 using generator-angular 0.15.1
+// Generated on 2017-03-11 using generator-angular 0.16.0
 'use strict';
 
 // # Globbing
@@ -8,6 +8,7 @@
 // 'test/spec/**/*.js'
 
 module.exports = function(grunt) {
+
     grunt.loadNpmTasks('grunt-build-control');
     var pkg = require('./package.json');
 
@@ -30,7 +31,6 @@ module.exports = function(grunt) {
     // Define the configuration for all the tasks
     grunt.initConfig({
 
-
         // Project settings
         yeoman: appConfig,
 
@@ -51,9 +51,9 @@ module.exports = function(grunt) {
                 files: ['test/spec/{,*/}*.js'],
                 tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
             },
-            styles: {
-                files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-                tasks: ['newer:copy:styles', 'postcss']
+            compass: {
+                files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+                tasks: ['compass:server', 'postcss:server']
             },
             gruntfile: {
                 files: ['Gruntfile.js']
@@ -222,6 +222,39 @@ module.exports = function(grunt) {
                         }
                     }
                 }
+            },
+            sass: {
+                src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+                ignorePath: /(\.\.\/){1,2}bower_components\//
+            }
+        },
+
+        // Compiles Sass to CSS and generates necessary files if requested
+        compass: {
+            options: {
+                sassDir: '<%= yeoman.app %>/styles',
+                cssDir: '.tmp/styles',
+                generatedImagesDir: '.tmp/images/generated',
+                imagesDir: '<%= yeoman.app %>/images',
+                javascriptsDir: '<%= yeoman.app %>/scripts',
+                fontsDir: '<%= yeoman.app %>/styles/fonts',
+                importPath: './bower_components',
+                httpImagesPath: '/images',
+                httpGeneratedImagesPath: '/images/generated',
+                httpFontsPath: '/styles/fonts',
+                relativeAssets: false,
+                assetCacheBuster: false,
+                raw: 'Sass::Script::Number.precision = 10\n'
+            },
+            dist: {
+                options: {
+                    generatedImagesDir: '<%= yeoman.dist %>/images/generated'
+                }
+            },
+            server: {
+                options: {
+                    sourcemap: true
+                }
             }
         },
 
@@ -343,7 +376,7 @@ module.exports = function(grunt) {
         ngtemplates: {
             dist: {
                 options: {
-                    module: 'myApp',
+                    module: 'realeyesAppApp',
                     htmlmin: '<%= htmlmin.dist.options %>',
                     usemin: 'scripts/scripts.js'
                 },
@@ -394,8 +427,8 @@ module.exports = function(grunt) {
                     src: ['generated/*']
                 }, {
                     expand: true,
-                    cwd: 'bower_components/bootstrap/dist',
-                    src: 'fonts/*',
+                    cwd: '.',
+                    src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
                     dest: '<%= yeoman.dist %>'
                 }]
             },
@@ -410,18 +443,17 @@ module.exports = function(grunt) {
         // Run some tasks in parallel to speed up the build process
         concurrent: {
             server: [
-                'copy:styles'
+                'compass:server'
             ],
             test: [
-                'copy:styles'
+                'compass'
             ],
             dist: [
-                'copy:styles',
+                'compass:dist',
                 'imagemin',
                 'svgmin'
             ]
         },
-
         buildcontrol: {
             options: {
                 dir: 'dist',
@@ -501,7 +533,7 @@ module.exports = function(grunt) {
         'copy:dist',
         'cdnify',
         'cssmin',
-        // 'uglify',
+        //      'uglify',
         'filerev',
         'usemin',
         'htmlmin'
